5417a439a30339d495f1248bad112fdd8fd105ec
Your Name <you@example.com>
2024-10-24 10:11:45 +0200

    changed to array

--- Main.java
+++ Main.java
@@ -5,10 +5,13 @@ import processing.core.PApplet;
 public class Main extends PApplet {
 	int x;
 	int y;
+	int anz;
+	int speed;
 	int diameter;
-	int futterX;
-	int futterY;
-	boolean isVisible;
+	int futterX[];
+	int futterY[];
+	boolean isVisible[];
+
 	public static void main(String[] args) {
 		PApplet.main("processingMaven.Main");
 	}
@@ -19,50 +22,59 @@ public class Main extends PApplet {
 
 	public void setup() {
 		background(255);
+		speed=15;
 		x = 300;
 		y = 300;
-		diameter=50;
-		isVisible=true;
-		futterX=(int)(Math.random()*width);
-		futterY=(int)(Math.random()*height);
+		diameter = 50;
+		anz = 5;
+		futterX = new int[anz];
+		futterY = new int[anz];
+		isVisible = new boolean[anz];
+		for (int i = 0; i < anz; i++) {
+			futterX[i] = (int) (Math.random() * width);
+			futterY[i] = (int) (Math.random() * height);
+			isVisible[i] = true;
+		}
+
 	}
 
 	public void draw() {
-		background(50,34,88);
-		fill(0,255,255);
+		background(50, 34, 88);
+		fill(0, 255, 255);
 		ellipse(x, y, diameter, diameter);
-		if(dist(x,y,futterX,futterY)<diameter/2) {
-			isVisible=false;
-		}
-		if(isVisible) {
-			ellipse(futterX,futterY,10,10);
+		for (int i = 0; i < anz; i++) {
+			if (dist(x, y, futterX[i], futterY[i]) < diameter / 2) {
+				isVisible[i] = false;
+			}
+			if (isVisible[i]) {
+				ellipse(futterX[i], futterY[i], 10, 10);
+			}
 		}
 		
+
 	}
 
-
-
 	public void keyPressed() {
-		
+
 		switch (key) {
 		case 'a':
-			x-=1;
-			x=constrain(x, diameter/2, width-diameter/2);
+			x -= speed;
+			x = constrain(x, diameter / 2, width - diameter / 2);
 			println(key);
 			break;
 		case 's':
-			y+=1;
-			y=constrain(y, diameter/2, height-diameter/2);
+			y += speed;
+			y = constrain(y, diameter / 2, height - diameter / 2);
 			println(key);
 			break;
 		case 'd':
-			x+=1;
-			x=constrain(x, diameter/2, width-diameter/2);
+			x += speed;
+			x = constrain(x, diameter / 2, width - diameter / 2);
 			println(key);
 			break;
 		case 'w':
-			y-=1;
-			y=constrain(y, diameter/2, height-diameter/2);
+			y -= speed;
+			y = constrain(y, diameter / 2, height - diameter / 2);
 			println(key);
 			break;
 		default:
